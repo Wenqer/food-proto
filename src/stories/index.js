9d1d123a97a25572a5e00743dfe9e200d6c3f079
yvoloshchuk <yaroslav.voloshchuk@grammarly.com>
2016-04-29 11:11:18 +0300

    update deps

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,23 +3,23 @@ import { storiesOf, action } from '@kadira/storybook'
 import Button from '../index'
 import Order from '../order'
 
-storiesOf('Button', module)
-  .add('default view', () => (
-    <Button onClick={ action('button clicked') }>Hello</Button>
-  ))
-  .add('some emojies as the text', () => (
-    <Button>😀 😎 👍 💯</Button>
-  ))
-  .add('custom styles', () => {
-    const style = {
-      fontSize: 20,
-      textTransform: 'uppercase',
-      color: '#FF8833',
-    }
-    return (
-      <Button style={ style }>Hello</Button>
-    )
-  })
+// storiesOf('Button', module)
+//   .add('default view', () => (
+//     <Button onClick={ action('button clicked') }>Hello</Button>
+//   ))
+//   .add('some emojies as the text', () => (
+//     <Button>😀 😎 👍 💯</Button>
+//   ))
+//   .add('custom styles', () => {
+//     const style = {
+//       fontSize: 20,
+//       textTransform: 'uppercase',
+//       color: '#FF8833',
+//     }
+//     return (
+//       <Button style={ style }>Hello</Button>
+//     )
+//   })
 
 storiesOf('Order', module)
   .add('default view', () => (
